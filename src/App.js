483fda356691c443b0daed3e8ce9ce9f9b6f6066
dmozzoni <cyric@hotmail.com>
2017-02-04 11:57:12 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createStore } from 'redux';
+
+
+
+const defaultState = { checked: false };
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+const store = createStore(reducer);
+
+
+
+class App extends React.Component {
+  constructor() {
+  super();
+  this.state = {};
+}
+
+componentWillMount() {
+  store.subscribe(() => this.setState(store.getState()));
+}
+
+render() {
+  const onClick = () => store.dispatch({ type: 'TOGGLE' });
+  return (
+    <div>
+      <h1>To-dos</h1>
+      <div>
+        Learn Redux&nbsp;
+        <input
+          type="checkbox"
+          checked={!!this.state.checked}
+          onClick={onClick} />
+      </div>
+      {
+        this.state.checked ? (<h2>Done!</h2>) : null
+      }
+    </div>
+  );
+}
+}
+
+export default App;
