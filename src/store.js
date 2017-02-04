3da1dc7c7710f641b1f05f36fef13a6de4bc8aac
dmozzoni <cyric@hotmail.com>
2017-02-04 17:50:46 -0500

    step 6

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { promiseMiddleware, localStorageMiddleware } from './middleware';
-import auth from './reducers/auth.js';
-import common from './reducers/common.js';
-import home from './reducers/home.js';
+import auth from './reducers/auth';
+import common from './reducers/common';
+import home from './reducers/home';
+import settings from './reducers/settings';
 
 const reducer = combineReducers({
   auth,
   common,
-  home
+  home,
+  settings
 });
 
 const middleware = applyMiddleware(promiseMiddleware, localStorageMiddleware);
